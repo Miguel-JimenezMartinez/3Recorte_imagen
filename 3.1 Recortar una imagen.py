077315778063a0f5b83bf516b06bdf40839eb96e
MiguelMike54 <66104861+MiguelMike54@users.noreply.github.com>
2020-06-03 22:51:46 -0500

    Update 3.1 Recortar una imagen.py

--- 3.1 Recortar una imagen.py
+++ 3.1 Recortar una imagen.py
@@ -6,7 +6,7 @@ imagen = cv2.imread('imagen.jpg')          #abrimos la imagen
 
 largo=imagen.shape[1]                      #Extraigo el largo de la imagen
 
-alto=imagen.shape[0]                       #Extraigo el ancho de la imagen
+alto=imagen.shape[0]                       #Extraigo el alto de la imagen
 
 corte=imagen[0:alto//2, 0:largo]           #Hacemos el corte, o mas bien la redimesion de mi matriz [empiezo en y:termino en y+altura, empiezo en x:termino en x+largo ]
 
