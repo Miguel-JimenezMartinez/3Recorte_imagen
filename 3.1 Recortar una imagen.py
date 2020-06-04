3b2a4813bd535851f2addcaa7f088f651b410905
MiguelMike54 <66104861+MiguelMike54@users.noreply.github.com>
2020-06-03 22:47:11 -0500

    Recorte una imagen sin usar .crop

---
+++ 3.1 Recortar una imagen.py
@@ -0,0 +1,20 @@
+import cv2
+import numpy as np                         #Importamos Numpy
+
+
+imagen = cv2.imread('imagen.jpg')          #abrimos la imagen
+
+largo=imagen.shape[1]                      #Extraigo el largo de la imagen
+
+alto=imagen.shape[0]                       #Extraigo el ancho de la imagen
+
+corte=imagen[0:alto//2, 0:largo]           #Hacemos el corte, o mas bien la redimesion de mi matriz [empiezo en y:termino en y+altura, empiezo en x:termino en x+largo ]
+
+cv2.imwrite('imagen_recortada.jpg',corte)  #Usamos esta funcion para guardarla en jpg
+
+cv2.imshow("Imagen recortada", corte)      #Muestro la imagen
+
+cv2.waitKey(0) 
+
+
+
